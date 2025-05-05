d83bcd6298e199ad7dc91316a51506859a0227e1
Your Name <you@example.com>
2025-05-05 23:26:43 +0500

    Add new LLM models and switch port to 7000

--- main.go
+++ main.go
@@ -36,8 +36,8 @@ func main() {
 		os.Exit(0)
 	}()
 
-	log.Println("Starting server on :8080")
-	if err := srv.Start(":8080"); err != nil {
+	log.Println("Starting server on :7000")
+	if err := srv.Start(":7000"); err != nil {
 		log.Fatalf("Server error: %v", err)
 	}
 }
